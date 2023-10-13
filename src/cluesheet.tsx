6bd83f932c08ba86187379c40a2d74a193bfc3c0
Jim Nguyen <jimnguyen77@gmail.com>
2023-10-13 08:23:19 -0700

    save game data to cookie in case of accidental refreshes; add a reset button to clear it

--- src/cluesheet.tsx
+++ src/cluesheet.tsx
@@ -1,4 +1,5 @@
 import { FC, useState } from 'react';
+import Cookies from 'js-cookie';
 
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { Box, Button, Container, Grid, TextField } from '@mui/material';
@@ -15,12 +16,35 @@ import {
 } from './common/styles.constants';
 
 const ClueSheet: FC = () => {
-  const [boxes, setBoxes] = useState(
-    Array(PLAYERS.length + WEAPONS.length + ROOMS.length).fill(
-      Array(NumBoxes.CHECK_BOXES).fill(false),
-    ),
+  const defaultBoxes = Array(PLAYERS.length + WEAPONS.length + ROOMS.length).fill(
+    Array(NumBoxes.CHECK_BOXES).fill(false),
   );
+
+  const defaultTextBoxes = Array(NumBoxes.PLAYERS).fill('');
+
+  const initialState = {
+    boxes: defaultBoxes,
+    textBoxes: defaultTextBoxes,
+  };
+
+  const [state, setState] = useState(() => {
+    const savedState = Cookies.get('gameState');
+    return savedState ? JSON.parse(savedState) : initialState;
+  });
+
   const theme = useTheme();
+
+  const saveStateToCookies = (newState: typeof state) => {
+    Cookies.set('gameState', JSON.stringify(newState), { expires: 1 }); // expires in 1 day
+    setState(newState);
+  };
+
+  const resetState = () => {
+    Cookies.remove('gameState');
+    setState(initialState);
+  };
+
+  console.log(state);
 
   return (
     <Container component='main' maxWidth='xs' sx={mainStyle}>
@@ -29,47 +53,56 @@ const ClueSheet: FC = () => {
           <Grid item xs={5} sx={gridTitleStyle(theme)}>
             {Titles.PLAYERS}
           </Grid>
-          {Array(NumBoxes.PLAYERS)
-            .fill(null)
-            .map((_, index) => (
-              <Grid item xs={1} key={index} sx={gridBorderStyle(theme)}>
-                <TextField
-                  size='small'
-                  inputProps={{ maxLength: 1 }}
-                  variant='standard'
-                  sx={TextFieldStyle}
-                />
-              </Grid>
-            ))}
+          {state.textBoxes.map((text: string, index: number) => (
+            <Grid item xs={1} key={index} sx={gridBorderStyle(theme)}>
+              <TextField
+                size='small'
+                inputProps={{ maxLength: 1 }}
+                value={text}
+                onChange={(e) => {
+                  const newTextboxes = [...state.textBoxes];
+                  newTextboxes[index] = e.target.value;
+                  saveStateToCookies({ ...state, textBoxes: newTextboxes });
+                }}
+                variant='standard'
+                sx={TextFieldStyle}
+              />
+            </Grid>
+          ))}
 
           <Section
             title={Titles.WHO}
             items={PLAYERS}
             theme={theme}
             rowIndex={0}
-            boxes={boxes}
-            setBoxes={setBoxes}
+            boxes={state.boxes}
+            setBoxes={(boxes) => saveStateToCookies({ ...state, boxes })}
           />
           <Section
             title={Titles.WHAT}
             items={WEAPONS}
             theme={theme}
             rowIndex={PLAYERS.length}
-            boxes={boxes}
-            setBoxes={setBoxes}
+            boxes={state.boxes}
+            setBoxes={(boxes) => saveStateToCookies({ ...state, boxes })}
           />
           <Section
             title={Titles.WHERE}
             items={ROOMS}
             theme={theme}
             rowIndex={PLAYERS.length + WEAPONS.length}
-            boxes={boxes}
-            setBoxes={setBoxes}
+            boxes={state.boxes}
+            setBoxes={(boxes) => saveStateToCookies({ ...state, boxes })}
           />
         </Grid>
       </Box>
-      <Box sx={{ my: 2 }}>
+      <Box sx={{ my: 2, display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+        <Button onClick={resetState} size='small' variant='contained' color='info'>
+          Reset
+        </Button>
+
         <Button
+          color='info'
           href={GitHubLink}
           rel='noopener noreferrer'
           size='small'
